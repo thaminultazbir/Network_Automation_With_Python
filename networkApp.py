6864f70b90ec95c70ca741cbea523879be6ba843
thaminultazbir <thaminultazbir1999@gmail.com>
2023-09-06 14:11:52 +0600

    ip address validity check

--- networkApp.py
+++ networkApp.py
@@ -1,6 +1,9 @@
 import sys
 
 from ip_file_valid import ip_file_valid
+from ip_addr_valid import ip_addr_valid
 
 ip_list = ip_file_valid()
-print(ip_list)
+# print(ip_list)
+ip = ip_addr_valid(ip_list)
+print(ip)
