884108dee82c27a9c5ec9a793f675b349f8e9dae
thaminultazbir <thaminultazbir1999@gmail.com>
2023-09-06 14:29:58 +0600

    Successfully ping in Switch

--- networkApp.py
+++ networkApp.py
@@ -2,8 +2,10 @@ import sys
 
 from ip_file_valid import ip_file_valid
 from ip_addr_valid import ip_addr_valid
+from ip_reach import ip_reach
 
 ip_list = ip_file_valid()
-# print(ip_list)
-ip = ip_addr_valid(ip_list)
-print(ip)
+# ip = ip_addr_valid(ip_list)
+
+# ip_reach(ip_list)
+
