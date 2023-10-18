7664963b8053b39c5bef52ecc347d01a0549c6f1
thaminultazbir <thaminultazbir1999@gmail.com>
2023-10-18 23:18:08 +0600

    connect with ssh

--- ssh_connection.py
+++ ssh_connection.py
@@ -33,7 +33,7 @@ def ssh_connection(ip):
     try:
         selected_user_file = open(user_file, 'r')
         selected_user_file.seek(0)
-        user_name = selected_user_file.readlines()[0].split(',')[0].rsplit("\n")
+        username = selected_user_file.readlines()[0].split(',')[0].rsplit("\n")
         selected_user_file.seek(0)
         password = selected_user_file.readlines()[0].split(',')[1].rsplit("\n")
 
@@ -41,3 +41,38 @@ def ssh_connection(ip):
         # -------login into the device----------
         session = paramiko.SSHClient()
         session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+        connection = session.invoke_shell()
+        connection.send("enable\n")
+        connection.send("terminal length 0\n")  #disable pagination
+        time.sleep(1)
+
+        # --Entering Global Configure Mode--
+        connection.send("\n")
+        connection.send("configure terminal\n")
+        time.sleep(1)
+
+        selected_cmd_file = open(cmd_file, 'r')
+        selected_cmd_file.seek(0)
+
+        for each_line in selected_cmd_file.readlines():
+            connection.send(each_line+'\n')
+            time.sleep(2)
+        
+        selected_user_file.close()
+        selected_cmd_file.close()
+
+        router_output = connection.recv(65535)
+
+        if re.search(b"% Invalid input", router_output):
+            print("* There is at least One IOS syntax error on device {} : (".format(ip))
+        else:
+            print("\n Done for Device {} :)".format(ip))
+        
+        print(str(router_output)+"\n")
+        session.close()
+
+    except paramiko.AuthenticationException:
+        print("Invalid Username/Password. Please check the credential file")
+        print("Closing Program Bye...!")
