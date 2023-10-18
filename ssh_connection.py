0720ff3a77bb73a2056f4aa993b57d8dd66f1d02
thaminultazbir <thaminultazbir1999@gmail.com>
2023-10-18 22:39:52 +0600

    command file checking

--- ssh_connection.py
+++ ssh_connection.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+# -------------Checking User Credentials--------------------
+
 user_file = input("\n# Enter user file path and name (e.g. D:\\MyApps\\myfile.txt): ")
 
 if os.path.isfile(user_file) == True:
@@ -9,3 +11,13 @@ if os.path.isfile(user_file) == True:
 else:
     print("\n* File {} does not exist :( Please check and try again.\n".format(user_file))
     sys.exit()
+
+#-------------------Checking Command File-------------------------
+
+cmd_file = input("\n# Enter command file path and name (e.g. D:\\MyApps\\myfile.txt): ")
+
+if os.path.isfile(cmd_file) == True:
+    print("\n* Command file is valid :)\n")
+
+else:
+    print("\n* File {} does not exist :( Please check and try again.\n".format(cmd_file))
